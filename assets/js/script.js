12911babd9c1d73b163b093160164c2b87370133
Alena Guillaume <91268509+AlenaGM@users.noreply.github.com>
2022-01-22 21:07:04 +0100

    finished css

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+
+
+document.getElementById("userEmail").className += " filled";
+document.getElementById("userPassword").className += " filled";
+document.getElementById("userFirstName").className += " filled";
+document.getElementById("userLastName").className += " filled";
+document.getElementById("userAddress").className += " filled";
+document.getElementById("userPostcode").className += " filled";
+document.getElementById("userCity").className += " filled";
+document.getElementById("userCountry").className += " filled";
+document.getElementById("userPhone").className += " filled";
+
+
+
+
+
+
+
+
+
