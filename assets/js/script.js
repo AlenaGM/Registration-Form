f3efbedfc426711251a5e97336031508b8686f69
AlenaGM <alena.guillaume4@gmail.com>
2022-01-23 02:25:52 +0100

    improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,121 @@
-document.getElementById("userEmail").className += " filled";
-document.getElementById("userPassword").className += " filled";
+let userEmail = document.getElementById("userEmail").value;
+let userPassword = document.getElementById("userPassword").value;
+let userFirstName = document.getElementById("userFirstName").value;
+let userLastName = document.getElementById("userLastName").value;
+let userAddress = document.getElementById("userAddress").value;
+let userPostcode = document.getElementById("userPostcode").value;
+let userCity = document.getElementById("userCity").value;
+let userCountry = document.getElementById("userCountry").value;
+let userPhone = document.getElementById("userPhone").value;
+
+
+if (userEmail == '') {;
+    document.getElementById("userEmail").className += " filled";
+};
+
+if (userPassword == '') {;
+    document.getElementById("userPassword").className += " filled";
+};
+
+if (userFirstName == '') {;
+    document.getElementById("userFirstName").className += " filled";
+};
+
+if (userLastName == '') {;
+    document.getElementById("userLastName").className += " filled";
+};
+
+if (userAddress == '') {;
+    document.getElementById("userAddress").className += " filled";
+};
+
+if (userPostcode == '') {;
+    document.getElementById("userPostcode").className += " filled";
+};
+
+if (userCity == '') {;
+    document.getElementById("userCity").className += " filled";
+};
+
+if (userCountry == '') {;
+    document.getElementById("userCountry").className += " filled";
+};
+
+if (userPhone == '') {;
+    document.getElementById("userPhone").className += " filled";
+};
+
+
+/*document.getElementById("userPassword").className += " filled";
 document.getElementById("userFirstName").className += " filled";
 document.getElementById("userLastName").className += " filled";
 document.getElementById("userAddress").className += " filled";
 document.getElementById("userPostcode").className += " filled";
 document.getElementById("userCity").className += " filled";
 document.getElementById("userCountry").className += " filled";
-document.getElementById("userPhone").className += " filled";
+document.getElementById("userPhone").className += " filled";*/
+/*
+let userEmail = document.getElementById("userEmail").value;
+let userPassword = document.getElementById("userPassword").value;
+let userFirstName = document.getElementById("userFirstName").value;
+let userLastName = document.getElementById("userLastName").value;
+let userAddress = document.getElementById("userAddress").value;
+let userPostcode = document.getElementById("userPostcode").value;
+let userCity = document.getElementById("userCity").value;
+let userCountry = document.getElementById("userCountry").value;
+let userPhone = document.getElementById("userPhone").value;
+
+function check(){
+
+    let userEmail = document.getElementById("userEmail").value;
+let userPassword = document.getElementById("userPassword").value;
+let userFirstName = document.getElementById("userFirstName").value;
+let userLastName = document.getElementById("userLastName").value;
+let userAddress = document.getElementById("userAddress").value;
+let userPostcode = document.getElementById("userPostcode").value;
+let userCity = document.getElementById("userCity").value;
+let userCountry = document.getElementById("userCountry").value;
+let userPhone = document.getElementById("userPhone").value;
+
+if (userEmail == '' || userPassword == '' || userFirstName == '' || userLastName == '' || userAddress == '' || userPostcode == '' || userCity == '' || userCountry == ''|| userPhone == '') {
+    alert('hihihihihihi');
+};
+
+  };
 
 
+/*
+let check = () => {
+
+    let username = document.getElementById("userFirstname").value;
+    let userphone = document.getElementById("userPhone").value;
+    let usermail = document.getElementById("userEmail").value;
+    let servicetype = document.getElementById("userLastName").value;
+    let comment = document.getElementById("userPassword").value;
+    document.getElementById('errorMessage').innerHTML = '';
+
+    if (userphone == '' || username == '' || usermail == '' || servicetype == '' || comment == '') {
+        document.getElementById('errorMessage').innerHTML = "Не все поля ввода заполнены!<br>";
+    };
+
+    else {
+        document.getElementById('errorMessage').innerHTML = `${username} , Ваш запрос отправлен!<br>`;
+    };
+}*/
+
+/*
+"use strict"
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('form');
     form.addEventListener('submit', formSend);
 
     async function formSend(e) {
+
         e.preventDefault();
 
         let error = formValidate(form);
 
         let formData = new formData(form);
-
 
         if (error===0){
             form.classList.add('_sending');
@@ -27,6 +123,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 method: 'POST',
                 body: formData
             });
+
             if(response.ok){
                 let result = await response.json();
                 alert(result.message);
@@ -77,10 +174,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     function emailTest(input) {
-        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(w{2,8})+$/.test(input.value);
-        /*return /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/.test(input.value);*/
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+        /*return /^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/.test(input.value);
     }
-});
+});*/
 
 
 
