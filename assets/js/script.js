2dd3ccc9d842db3946f8819316a53f6139f2ab2d
AlenaGM <alena.guillaume4@gmail.com>
2022-01-25 14:21:28 +0100

    better writing

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,107 +10,107 @@ let userPhone = document.getElementById("userPhone");
 
 /*Для оформления*/
 let addFilledEmail = () =>{
-    document.getElementById("userEmail").classList.add ('filled');
+    userEmail.classList.add ('filled');
     document.getElementById('emailRequired').innerHTML = '';
 
     if (userEmail.value != '') {
-        document.getElementById("userEmail").classList.add ('input_valid');
+        userEmail.classList.add ('input_valid');
     } else {
-        document.getElementById("userEmail").classList.remove ('input_valid');
-        document.getElementById("userEmail").classList.remove ('filled');
+        userEmail.classList.remove ('input_valid');
+        userEmail.classList.remove ('filled');
     };
 }
 
 let addFilledPassword = () =>{
-    document.getElementById("userPassword").classList.add ('filled');
+    userPassword.classList.add ('filled');
     document.getElementById('passwordRequired').innerHTML = '';
 
     if (userPassword.value != '') {
-        document.getElementById("userPassword").classList.add ('input_valid');
+        userPassword.classList.add ('input_valid');
     } else {
-        document.getElementById("userPassword").classList.remove ('input_valid');
-        document.getElementById("userPassword").classList.remove ('filled');
+        userPassword.classList.remove ('input_valid');
+        userPassword.classList.remove ('filled');
     };
 }
 
 let addFilledFirstName = () =>{
-    document.getElementById("userFirstName").classList.add ('filled');
+    userFirstName.classList.add ('filled');
     document.getElementById('firstNameRequired').innerHTML = '';
 
     if (userFirstName.value != '') {
-        document.getElementById("userFirstName").classList.add ('input_valid');
+        userFirstName.classList.add ('input_valid');
     } else {
-        document.getElementById("userFirstName").classList.remove ('input_valid');
-        document.getElementById("userFirstName").classList.remove ('filled');
+        userFirstName.classList.remove ('input_valid');
+        userFirstName.classList.remove ('filled');
     };
 }
 
 let addFilledLastName = () =>{
-    document.getElementById("userLastName").classList.add ('filled');
+    userLastName.classList.add ('filled');
     document.getElementById('lastNameRequired').innerHTML = '';
 
     if (userLastName.value != '') {
-        document.getElementById("userLastName").classList.add ('input_valid');
+        userLastName.classList.add ('input_valid');
     } else {
-        document.getElementById("userLastName").classList.remove ('input_valid');
-        document.getElementById("userLastName").classList.remove ('filled');
+        userLastName.classList.remove ('input_valid');
+        userLastName.classList.remove ('filled');
     };
 }
 
 let addFilledAddress = () =>{
-    document.getElementById("userAddress").classList.add ('filled');
+    userAddress.classList.add ('filled');
 
     if (userAddress.value != '') {
-        document.getElementById("userAddress").classList.add ('input_valid');
+        userAddress.classList.add ('input_valid');
     } else {
-        document.getElementById("userAddress").classList.remove ('input_valid');
-        document.getElementById("userAddress").classList.remove ('filled');
+        userAddress.classList.remove ('input_valid');
+        userAddress.classList.remove ('filled');
     };
 }
 
 let addFilledPostcode = () =>{
-    document.getElementById("userPostcode").classList.add ('filled');
+    userPostcode.classList.add ('filled');
 
     if (userPostcode.value != '') {
-        document.getElementById("userPostcode").classList.add ('input_valid');
+        userPostcode.classList.add ('input_valid');
     } else {
-        document.getElementById("userPostcode").classList.remove ('input_valid');
-        document.getElementById("userPostcode").classList.remove ('filled');
+        userPostcode.classList.remove ('input_valid');
+        userPostcode.classList.remove ('filled');
     };
 }
 
 let addFilledCity = () =>{
-    document.getElementById("userCity").classList.add ('filled');
+    userCity.classList.add ('filled');
 
     if (userCity.value != '') {
-        document.getElementById("userCity").classList.add ('input_valid');
+        userCity.classList.add ('input_valid');
     } else {
-        document.getElementById("userCity").classList.remove ('input_valid');
-        document.getElementById("userCity").classList.remove ('filled');
+        userCity.classList.remove ('input_valid');
+        userCity.classList.remove ('filled');
     };
 }
 
 let addFilledCountry = () =>{
-    document.getElementById("userCountry").classList.add ('filled');
+    userCountry.classList.add ('filled');
     document.getElementById('countryRequired').innerHTML = '';
 
     if (userCountry.value != '') {
-        document.getElementById("userCountry").classList.add ('input_valid');
+        userCountry.classList.add ('input_valid');
     } else {
-        document.getElementById("userCountry").classList.remove ('input_valid');
-        document.getElementById("userCountry").classList.remove ('filled');
+        userCountry.classList.remove ('input_valid');
+        userCountry.classList.remove ('filled');
     };
 }
 
 let addFilledPhone = () =>{
-    document.getElementById("userPhone").classList.add ('filled');
+    userPhone.classList.add ('filled');
     document.getElementById('phoneRequired').innerHTML = '';
 
     if (userPhone.value != '') {
-        document.getElementById("userPhone").classList.add ('input_valid');
+        userPhone.classList.add ('input_valid');
     } else {
-        document.getElementById("userPhone").classList.remove ('input_valid');
-        document.getElementById("userPhone").classList.remove ('filled');
+        userPhone.classList.remove ('input_valid');
+        userPhone.classList.remove ('filled');
     };
 }
 /*Конец оформления*/
@@ -130,48 +130,48 @@ function checkValidity() {
 
     /*Украшательство*/
     if (userEmail.value == '') {
-        document.getElementById("userEmail").classList.remove ('input_valid');
-        document.getElementById("userEmail").classList.add ('input_error');
+        userEmai.classList.remove ('input_valid');
+        userEmail.classList.add ('input_error');
         document.getElementById('emailRequired').innerHTML = 'Email is required';
     } else {
         document.getElementById('emailRequired').innerHTML = '';
     };
 
     if (userPassword.value == '') {
-        document.getElementById("userPassword").classList.remove ('input_valid');
-        document.getElementById("userPassword").classList.add ('input_error');
+        userPassword.classList.remove ('input_valid');
+        userPassword.classList.add ('input_error');
         document.getElementById('passwordRequired').innerHTML = 'Password is required';
     } else {
         document.getElementById('passwordRequired').innerHTML = '';
     };
 
     if (userFirstName.value == '') {
-        document.getElementById("userFirstName").classList.remove ('input_valid');
-        document.getElementById("userFirstName").classList.add ('input_error');
+        userFirstName.classList.remove ('input_valid');
+        userFirstName.classList.add ('input_error');
         document.getElementById('firstNameRequired').innerHTML = 'First name is required';
     } else {
         document.getElementById('firstNameRequired').innerHTML = '';
     };
 
     if (userLastName.value == '') {
-        document.getElementById("userLastName").classList.remove ('input_valid');
-        document.getElementById("userLastName").classList.add ('input_error');
+        userLastName.classList.remove ('input_valid');
+        userLastName.classList.add ('input_error');
         document.getElementById('lastNameRequired').innerHTML = 'Last name is required';
     } else {
         document.getElementById('lastNameRequired').innerHTML = '';
     };
 
     if (userCountry.value == '') {
-        document.getElementById("userCountry").classList.remove ('input_valid');
-        document.getElementById("userCountry").classList.add ('input_error');
+        userCountry.classList.remove ('input_valid');
+        userCountry.classList.add ('input_error');
         document.getElementById('countryRequired').innerHTML = 'Country is required';
     } else {
         document.getElementById('countryRequired').innerHTML = '';
     };
 
     if (userPhone.value == '') {
-        document.getElementById("userPhone").classList.remove ('input_valid');
-        document.getElementById("userPhone").classList.add ('input_error');
+        userPhone.classList.remove ('input_valid');
+        userPhone.classList.add ('input_error');
         document.getElementById('phoneRequired').innerHTML = 'Phone is required';
     } else {
         document.getElementById('phoneRequired').innerHTML = '';
